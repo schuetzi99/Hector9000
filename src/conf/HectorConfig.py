18f738c81d5abe58804d98b7df2a0e3758bedd71
schuetzi99 <klaus.schuetz@gmx.org>
2020-01-27 21:23:26 +0100

    werte aendern fuer james

--- src/conf/HectorConfig.py
+++ src/conf/HectorConfig.py
@@ -5,27 +5,28 @@ config = {
     "hx711": {
         "CLK": 29,
         "DAT": 31,
-        "ref":  869.6761904761905 # calibration yields 100 g <-> readout 214500
+        #"ref":  869.6761904761905 # charles calibration yields 100 g <-> readout 214500
+        "ref":  346.6917808219178 # james calibration yields 100 g <-> readout 214500
     },
     "pca9685": {
         "freq": 60,
         "valvechannels": range(12),  # 0..11
         "valvepositions": [  # (open, closed)
-            (375, 535),  # ch 0
+            (375, 562),  # ch 0
             (375, 560),  # ch 1
-            (375, 545),  # ch 2
-            (375, 575),  # ch 3
-            (375, 580),  # ch 4
-            (375, 550),  # ch 5
-            (375, 545),  # ch 6
-            (375, 555),  # ch 7
-            (375, 535),  # ch 8
-            (375, 555),  # ch 9
-            (375, 550),  # ch 10
-            (375, 560)  # ch 11
+            (375, 543),  # ch 2
+            (375, 565),  # ch 3
+            (375, 565),  # ch 4
+            (375, 562),  # ch 5
+            (375, 560),  # ch 6
+            (375, 568),  # ch 7
+            (375, 528),  # ch 8
+            (375, 552),  # ch 9
+            (375, 558),  # ch 10
+            (375, 575)  # ch 11
         ],
         "fingerchannel": 12,
-        "fingerpositions": (280, 380, 430),  # retracted, above bell, bell
+        "fingerpositions": (250, 350, 400),  # retracted, above bell, bell
         "lightpin": 22,
         "lightpwmchannel": 13,
         "lightpositions": (0, 500)
