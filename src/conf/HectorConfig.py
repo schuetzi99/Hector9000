557593d8e8d897fe8854a5f37d4c6a93ad81c114
schuetzi99 <klaus.schuetz@gmx.org>
2020-02-22 21:53:03 +0100

    Ventil 2 etwas mehr schliessen

--- src/conf/HectorConfig.py
+++ src/conf/HectorConfig.py
@@ -12,7 +12,7 @@ config = {
         "valvechannels": range(12),  # 0..11
         "valvepositions": [  # (open, closed)
             (375, 535),  # ch 0
-            (375, 560),  # ch 1
+            (375, 570),  # ch 1
             (375, 545),  # ch 2
             (375, 575),  # ch 3
             (375, 580),  # ch 4
